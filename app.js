9d7ed4627502ee05eb199c1a9e5835c3656d8b1f
Mary Martinez <mary.diana.martinez@gmail.com>
2022-01-31 18:00:41 -0800

    Inital form event listener

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // import functions and grab DOM elements
 
-// let state
+const form = document.getElementById('add-ingredient');
 
+// let state
+const ingredients = [];
 // set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// get user input
+// use user input to update state 
+// update DOM to reflect the new state
+form.addEventListener('click', (e) => {
+    e.preventDefault();
+    // Step 1 - add code to track each submission to the ingredients
+    const ingredient = new FormData.get(form));
+    // Create an object to "model" you statistic like the following:
+    // { player: 'Bob', points: 2 }
+
+});
